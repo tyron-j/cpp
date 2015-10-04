246500ad662f9029e1435786f7934a4619662134
Tyron Jung <tyronjung@hotmail.com>
2015-10-04 01:01:32 -0400

    String Example

--- Sandbox/Sandbox/Test.cpp
+++ Sandbox/Sandbox/Test.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
+#include <string>
 
 #include "Sandbox.h"
 
 void main() {
-	std::cout << "Hello world!" << std::endl;
+	std::string intStr = "100 144";
+	int a = std::stoi(intStr.substr(0, intStr.find(' ')));
+	int b = std::stoi(intStr.substr(intStr.find(' '), intStr.length()));
 
-	LinkedList * Pointer = new LinkedList(1); // point to new linked list
-	LinkedList& MyLinkedList = *Pointer; // get value by reference
-
-	MyLinkedList.Add(2);
-	MyLinkedList.Add(3);
-	MyLinkedList.Add(4);
-	MyLinkedList.Add(5);
-	MyLinkedList.Iterate();
-
-	std::cout << "The length of the linked list is: " << MyLinkedList.Length << std::endl;
-
-	delete Pointer;
+	std::cout << a << " " << b;
 
 	system("pause");
 }
